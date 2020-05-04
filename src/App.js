d6429f299475eb0f4e2298d490952ae004ffbb33
Karan Kanchetty <karan.kanchetty@jci.com>
2020-05-04 05:27:05 -0500

    Adding a Netlify build script

--- src/App.js
+++ src/App.js
@@ -1,34 +1,75 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
 import Routes from "./Routes";
 import "./App.css";
-import { Nav, NavItem } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { AppContext } from "./libs/contextLib";
+import { Auth } from "aws-amplify";
 
 function App() {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+  const history = useHistory();
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+  
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    }
+    catch(e) {
+      if (e !== 'No current user') {
+        alert(e);
+      }
+    }
+  
+    setIsAuthenticating(false);
+  }
+
+  async function handleLogout() {
+    await Auth.signOut();
+  
+    userHasAuthenticated(false);
+  
+    history.push("/login");
+  }
+
   return (
-    <div className="App container">
-      <Navbar fluid collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to="/">Scratch</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav pullRight>
-            <LinkContainer to="/signup">
-              <NavItem>Signup</NavItem>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <NavItem>Login</NavItem>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <Routes />
-    </div>
+    !isAuthenticating && (
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">Scratch</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              {isAuthenticated ? (
+                <>
+                  <NavItem href="/settings">Settings</NavItem>
+                  <NavItem onClick={handleLogout}>Logout</NavItem>
+                </>
+              ) : (
+                <>
+                  <NavItem href="/signup">Signup</NavItem>
+                  <NavItem href="/login">Login</NavItem>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <AppContext.Provider
+          value={{ isAuthenticated, userHasAuthenticated }}
+        >
+          <Routes />
+        </AppContext.Provider>
+      </div>
+    )
   );
 }
 
--- src/config.js
+++ src/config.js
@@ -1,16 +1,48 @@
-export default {
+  const dev = {
     s3: {
-      REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
-      BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+      REGION: "us-east-2",
+      BUCKET: "notes-appuploads"
     },
     apiGateway: {
-      REGION: "YOUR_API_GATEWAY_REGION",
-      URL: "YOUR_API_GATEWAY_URL"
+      REGION: "us-east-2",
+      URL: "https://2sxo0hfttf.execute-api.us-east-2.amazonaws.com/prod/"
     },
     cognito: {
-      REGION: "YOUR_COGNITO_REGION",
-      USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
-      APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
-      IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
-    }
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_9U1uOF5zv",
+      APP_CLIENT_ID: "bevgncsd4ue8grgrcvd8su4n1",
+      IDENTITY_POOL_ID: "us-east-2:5b58bbbe-a319-4da6-b758-319ddf38ae9f"
+    },
+    STRIPE_KEY: "pk_test_WdO2PzgoyRtvvcHgs4X048dH00JHMLX23",
+  };
+  
+  const prod = {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "notes-appuploads"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://2sxo0hfttf.execute-api.us-east-2.amazonaws.com/prod/"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_9U1uOF5zv",
+      APP_CLIENT_ID: "bevgncsd4ue8grgrcvd8su4n1",
+      IDENTITY_POOL_ID: "us-east-2:5b58bbbe-a319-4da6-b758-319ddf38ae9f"
+    },
+    STRIPE_KEY: "pk_test_WdO2PzgoyRtvvcHgs4X048dH00JHMLX23",
+  };
+  
+  console.log(process.env.REACT_APP_STAGE);
+
+  // Default to dev if not set
+  const config = process.env.REACT_APP_STAGE === 'prod'
+    ? prod
+    : dev;
+  
+  export default {
+    // Add common config values here
+    MAX_ATTACHMENT_SIZE: 5000000,
+    ...config
   };
